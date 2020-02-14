0742987d0a45b3588f4c9054dabd3cd8c147c608
Nekobitlz <jokers.bit@gmail.com>
2020-02-14 23:40:58 +0000

    Add main search fragment and layout for it

---
+++ app/src/main/java/com/nekobitlz/aviasales/features/search/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.nekobitlz.aviasales.features.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nekobitlz.aviasales.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): SearchFragment = SearchFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
