01de4b3851c87879eefebf6ee6158037b0377ff2
Nekobitlz <jokers.bit@gmail.com>
2020-02-15 22:48:39 +0000

    Add view model for search screen

---
+++ app/src/main/java/com/nekobitlz/aviasales/features/search/SearchViewModel.kt
@@ -0,0 +1,18 @@
+package com.nekobitlz.aviasales.features.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+
+    fun onDirectionFromClicked() {
+    }
+
+    fun onDirectionToClicked() {
+    }
+
+    fun onSearchClicked() {
+    }
+
+    fun onSwapClicked() {
+    }
+}
