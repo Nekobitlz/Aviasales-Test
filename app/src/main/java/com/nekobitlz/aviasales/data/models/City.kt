0ef8003c52a555c0435f5b012a89d655ae4236dc
Nekobitlz <jokers.bit@gmail.com>
2020-02-17 21:29:24 +0300

    Make city model serializable

--- app/src/main/java/com/nekobitlz/aviasales/data/models/City.kt
+++ app/src/main/java/com/nekobitlz/aviasales/data/models/City.kt
@@ -1,9 +1,11 @@
 package com.nekobitlz.aviasales.data.models
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class City(
     @SerializedName("city") val cityName: String,
     @SerializedName("country") val countryName: String? = null,
-    @SerializedName("countryCode") val countryCode: String
-)
+    @SerializedName("countryCode") val countryCode: String,
+    @SerializedName("location") val location: Location? = null
+) : Serializable
